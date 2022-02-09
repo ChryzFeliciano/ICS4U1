6af492e39270fbc2fde51af850cef06455b0a62e
Chryz Feliciano <45033374+ChryzFeliciano@users.noreply.github.com>
2022-02-08 21:10:25 -0500

    Delete Chapter 5/5.1 directory

--- Chapter 5/5.1/5.1 Q1.java
+++
@@ -1,9 +0,0 @@
-/*
-Explain the difference between a method definition and a method invocation.
-
-The method definition is what the method will do. 
-The method definition is a code that tells the compiler what to output. 
-The method definition can be complex and have multiple lines of code. 
-The method invocation occurs when we use the method from the main method.
-
-*/
